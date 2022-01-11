5e1d20ba07482fe384467b89cf0db2df49c31ff8
Jurgen Louvris <jurgen.louvris@student.rocva.nl>
2022-01-11 11:20:51 +0100

    innterHTML

---
+++ opdr12/functions.js
@@ -0,0 +1,8 @@
+<buttondocument.querySelector(".Knop1").innerHTML = 'Hier stond een knop';"vervang de knop"
+document.querySelector(".Knop2").innerHTML = 'Hier stond een knop';"vervang de knop"
+document.querySelector(".Knop3").innerHTML = 'Hier stond een knop';"vervang de knop"
+document.querySelector(".Knop4").innerHTML = 'Hier stond een knop';"vervang de knop"
+document.querySelector(".Knop5").innerHTML = 'Hier stond een knop';"vervang de knop"
+document.getElementById("Tekst1").innerHTML = 'Hier stond een header';"vervand de header"
+document.getElementById("Tekst2").innerHTML = 'Hier stond een header';"vervand de header"
+document.querySelector("body").innerHTML = "Alles weg";
